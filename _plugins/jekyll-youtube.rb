26d531bfb99dd9d51842c3e4b251d7fa9e080755
baramnemse <baramnemse@gmail.com>
2018-08-22 11:11:24 +0900

    Update jekyll-youtube.rb

--- _plugins/jekyll-youtube.rb
+++ _plugins/jekyll-youtube.rb
@@ -1,42 +1,27 @@
-# Title: Responsive Lazy Load YouTube embed tag for Jekyll
-# Author: Brett Terpstra <http://brettterpstra.com>
-# Description: Output a styled element for onClick replacement with responsive layout
-#
-# Syntax {% youtube video_id [width height] ["Caption"] %}
-#
-# Example:
-# {% youtube B4g4zTF5lDo 480 360 %}
-# {% youtube http://youtu.be/2NI27q3xNyI %}
+class YouTube < Liquid::Tag
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
-module Jekyll
-  class YouTubeTag < Liquid::Tag
-    @videoid = nil
-    @width = ''
-    @height = ''
+  def initialize(tagName, markup, tokens)
+    super
 
-    def initialize(tag_name, markup, tokens)
-      if markup =~ /(?:(?:https?:\/\/)?(?:www.youtube.com\/(?:embed\/|watch\?v=)|youtu.be\/)?(\S+)(?:\?rel=\d)?)(?:\s+(\d+)\s(\d+))?(?:\s+"(.*?)")?/i
-        @videoid = $1
-        @width = $2 || "480"
-        @height = $3 || "360"
-        @caption = $4 ? "<figcaption>#{$4}</figcaption>" : ""
-      end
-      super
-    end
+    if markup =~ Syntax then
+      @id = $1
 
-    def render(context)
-      context.environments.first['page']['youtube'] = @videoid
-      if @videoid
-        # Thanks to Andrew Clark for the inline CSS calculation idea <http://contentioninvain.com/2013/02/13/video-embeds-for-responsive-designs/>
-        intrinsic = ((@height.to_f / @width.to_f) * 100)
-        padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
-        video = %Q{<a class="youtube" href="https://www.youtube.com/watch?v=#{@videoid}" data-videoid="#{@videoid}" data-width="#{@width}" data-height="#{@height}">YouTube Video</a>}
-        %Q{<figure class="bt-video-container" style="padding-bottom:#{padding_bottom}">#{video}#{@caption}</figure>}
+      if $2.nil? then
+          @width = 560
+          @height = 420
       else
-        "Error processing input, expected syntax: {% youtube video_id [width height] %}"
+          @width = $2.to_i
+          @height = $3.to_i
       end
+    else
+      raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
-end
 
-Liquid::Template.register_tag('youtube', Jekyll::YouTubeTag)
+  def render(context)
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@id}?color=white&theme=light\"></iframe>"
+  end
+
+  Liquid::Template.register_tag "youtube", self
+end
